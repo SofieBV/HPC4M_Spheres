5c1df429eea1bb130aea420bd934df95d596bb73
Alix Leroy <A.leroy@sms.ed.ac.uk>
2023-03-10 09:41:54 +0000

    to do list update

--- Python code/first_try.py
+++ Python code/first_try.py
@@ -1,1 +1,17 @@
+#### To do list ####
+"""
+General steps: 
+
+commenting code really well 
+
+write the code in serial: 
+- square shape starting with 10 
+- create the heap structure (can just be called in python)
+- colisions between walls and colisions between particles 
+
+or initialise heap structure then compute colisions and build the heap structure 
+check if the code works properly (run it backward and see if we get the same initial conditions)
+parralelise the code and do fun things 
+
+"""
 import numpy as np
